5d4a33651b805be537fb7bf114fe0146cfbb1aa2
Phuckyucrypto <phuckyucrypto@gmail.com>
2024-08-14 12:23:18 -0600

    update WL phases

--- allowlist.tsx
+++ allowlist.tsx
@@ -6,20 +6,23 @@ export const allowLists = new Map<string, Array<string>> ([
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
       "D7rqGLTZkFUDgeyVTQioY9WTpKgDuWLQPQMhXaYrdhQr",
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
-      "ZupsZq7o5HdXjdH47PGgS56YaZJuvSkxu6mHnkd1nSJ"
+      "ZupsZq7o5HdXjdH47PGgS56YaZJuvSkxu6mHnkd1nSJ",
+      "74Ae8D9oJjo3CGgbBgFbZAHXHpJqUtTrzvYeZ6ftHbDA"
     ]],
     ["OG",
     [
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
       "D7rqGLTZkFUDgeyVTQioY9WTpKgDuWLQPQMhXaYrdhQr",
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
-      "ZupsZq7o5HdXjdH47PGgS56YaZJuvSkxu6mHnkd1nSJ"
+      "ZupsZq7o5HdXjdH47PGgS56YaZJuvSkxu6mHnkd1nSJ",
+      "74Ae8D9oJjo3CGgbBgFbZAHXHpJqUtTrzvYeZ6ftHbDA"
     ]],
     ["default",
     [
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
       "D7rqGLTZkFUDgeyVTQioY9WTpKgDuWLQPQMhXaYrdhQr",
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
-      "ZupsZq7o5HdXjdH47PGgS56YaZJuvSkxu6mHnkd1nSJ"
+      "ZupsZq7o5HdXjdH47PGgS56YaZJuvSkxu6mHnkd1nSJ",
+      "74Ae8D9oJjo3CGgbBgFbZAHXHpJqUtTrzvYeZ6ftHbDA"
     ]],
 ]);
