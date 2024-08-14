601c30886322e765ad5528f7165d3b7bf1b6f71d
Phuckyucrypto <phuckyucrypto@gmail.com>
2024-08-14 12:27:53 -0600

    update names for phases

--- allowlist.tsx
+++ allowlist.tsx
@@ -1,7 +1,7 @@
 //add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
 //You can also add them here and copy the route from the console log 
 export const allowLists = new Map<string, Array<string>> ([
-    ["WL",
+    ["FCFS",
     [
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
       "D7rqGLTZkFUDgeyVTQioY9WTpKgDuWLQPQMhXaYrdhQr",
@@ -9,7 +9,7 @@ export const allowLists = new Map<string, Array<string>> ([
       "ZupsZq7o5HdXjdH47PGgS56YaZJuvSkxu6mHnkd1nSJ",
       "74Ae8D9oJjo3CGgbBgFbZAHXHpJqUtTrzvYeZ6ftHbDA"
     ]],
-    ["OG",
+    ["GTD",
     [
       "4vRnHa9nLAYtRwjfk5eMdL6Lp6df31d8wasr18pUfMsz",
       "D7rqGLTZkFUDgeyVTQioY9WTpKgDuWLQPQMhXaYrdhQr",
